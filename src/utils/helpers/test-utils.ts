296626cf66b811394eec25a2feeeea745d5e3ed4
pfellow <pfellow@gmail.com>
2024-07-12 19:23:09 +1200

    add accessibility test, optimise component tests, add coverage to config

--- src/utils/helpers/test-utils.ts
+++ src/utils/helpers/test-utils.ts
@@ -1,1 +1,16 @@
-//TODO Put vuetify as global plugin here
+import type { Component } from 'vue';
+import { render as renderComponent } from '@testing-library/vue';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom/vitest';
+import vuetify from '@/plugins/vuetify';
+
+type RenderOptions = Parameters<typeof renderComponent>[1];
+
+export * from '@testing-library/vue';
+
+export const render = (ui: Component, options?: RenderOptions) => {
+  return {
+    ...renderComponent(ui, { ...options, global: { plugins: [vuetify] } }),
+    testUser: userEvent.setup()
+  };
+};
--- src/views/pages/maintenance/error/__tests__/Error404Page.test.ts
+++ src/views/pages/maintenance/error/__tests__/Error404Page.test.ts
@@ -1,17 +1,26 @@
-import { render, screen } from '@testing-library/vue';
+import { render, screen } from '@/utils/helpers/test-utils';
 import Error404Page from '../Error404Page.vue';
-import vuetify from '../../../../../plugins/vuetify';
+import { axe, toHaveNoViolations } from 'jest-axe';
+
+expect.extend(toHaveNoViolations);
 
 describe('Error404Page', () => {
   it('renders the component', () => {
-    render(Error404Page, { global: { plugins: [vuetify] } });
+    render(Error404Page);
 
     screen.getByText('Something is wrong');
   });
 
   it("doesn't have a test text", async () => {
-    render(Error404Page, { global: { plugins: [vuetify] } });
+    render(Error404Page);
 
     expect(() => screen.getByText('Everything is ok')).toThrow();
+  });
+
+  it('should be accessible', async () => {
+    const { container } = render(Error404Page);
+    const result = await axe(container);
+
+    expect(result).toHaveNoViolations();
   });
 });
